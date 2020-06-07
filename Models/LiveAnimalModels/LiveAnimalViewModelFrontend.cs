0bbdef28d171022291381823c86502136344fe55
Nayeem Ahmed <nayeemahmed24@gmail.com>
2020-06-07 19:28:03 +0600

    Inddex page bangla implemented

--- Models/LiveAnimalModels/LiveAnimalViewModelFrontend.cs
+++ Models/LiveAnimalModels/LiveAnimalViewModelFrontend.cs
@@ -11,16 +11,24 @@ namespace Models.LiveAnimalModels
 
         [Required]
         public string Title { get; set; }
+        [Required]
+        public string TitleBn { get; set; }
 
         [Required]
         public string Category { get; set; }
+        [Required]
+        public string CategoryBn { get; set; }
 
         [Required]
         public double Price { get; set; }
         public string Color { get; set; }
+        public string ColorBn { get; set; }
         public string Origin { get; set; }
+        public string OriginBn { get; set; }
         public string Description { get; set; }
+        public string DescriptionBn { get; set; }
         public string Location { get; set; }
+        public string LocationBn { get; set; }
         public bool Sold { get; set; }
         public bool Featured { get; set; }
         public List<string> Images { get; set; }
