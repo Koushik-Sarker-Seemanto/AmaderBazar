a35c7d25561923184142da1467d8c9556701b0f6
Nayeem Ahmed <nayeemahmed24@gmail.com>
2020-06-15 15:23:45 +0600

    icon and phone number validation implemented

--- Models/Entities/Order.cs
+++ Models/Entities/Order.cs
@@ -18,6 +18,9 @@ namespace Models.Entities
         [Required]
         public  string Name { get; set; }
         [Required]
+        [RegularExpression("^[0-9]*$", ErrorMessage = "PhoneNumber must be valid and all numeric")]
+        [MaxLength(11)]
+        [MinLength(11)]
         public string PhoneNumber { get; set; }
         public string Address { get; set; }
         [DefaultValue(false)]
